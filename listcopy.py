b3b9bf1eef942abc6bc80072373e5d715ab48e51
Manoel Neto <113383491+tbytecreator@users.noreply.github.com>
2022-12-21 23:09:42 -0300

    Create listcopy.py

---
+++ listcopy.py
@@ -0,0 +1,11 @@
+# ---------------------------------------
+# Para copiar uma lista use o metodo copy
+# ---------------------------------------
+first=[1,2,3,4,5]
+second=first
+third=first.copy() 
+first.append(6)
+print("First  =>",first)
+print("Second =>",second)
+print("Third  =>",third)
+# ---------------------------------------
